5733f3ba1d2f7295c36d1f76c2119bf80b5d4aa9
sunsun0312 <jessiesun0312@gmail.com>
2021-01-15 18:33:12 +1100

    update

---
+++ precache-manifest.3aa4beadd6996d611bb107dbd836de1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3727aec2fc7060f36127b8198e46b12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d576b0ec7a57ac635b5",
+    "url": "/static/css/2.2eceed1a.chunk.css"
+  },
+  {
+    "revision": "dec1bbe6e27797c01600",
+    "url": "/static/css/main.464f6e5b.chunk.css"
+  },
+  {
+    "revision": "8d576b0ec7a57ac635b5",
+    "url": "/static/js/2.2206adea.chunk.js"
+  },
+  {
+    "revision": "0db765a05ed05301f03f23cddceb3fd1",
+    "url": "/static/js/2.2206adea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec1bbe6e27797c01600",
+    "url": "/static/js/main.4089d886.chunk.js"
+  },
+  {
+    "revision": "68becbbe6ee5d102a90d",
+    "url": "/static/js/runtime-main.957bd2eb.js"
+  }
+]);
